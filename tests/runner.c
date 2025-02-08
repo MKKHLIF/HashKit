eddafd27b83b028790ad7714396d7214c32b4b8a
mk <khlif.mk@proton.me>
2025-02-09 00:53:23 +0100

    outsourcing sha1 tests :)

--- tests/runner.c
+++ tests/runner.c
@@ -1,9 +1,8 @@
 #include "unity.h"
 
-extern void test_md5(void);
-extern void test_sha1(void);
-extern void test_sha2(void);
-
+extern void test_hash_test_vectors(void);
+extern void test_hash_insanely_long_input(void);
+extern void test_hash_to_byte_vector(void);
 
 void setUp(void) {
 }
@@ -11,8 +10,13 @@ void setUp(void) {
 void tearDown(void) {
 }
 
+// Main function to run the tests
 int main(void) {
     UNITY_BEGIN();
+
+    RUN_TEST(test_hash_test_vectors);
+    RUN_TEST(test_hash_insanely_long_input);
+    RUN_TEST(test_hash_to_byte_vector);
 
     return UNITY_END();
 }
